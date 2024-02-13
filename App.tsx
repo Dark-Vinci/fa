22fea48eb1e4300b05c12a5c9319aece2ebb5eeb
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-02-13 10:36:31 +0000

    chore: more update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  const rHex = r.toString(16).padStart(2, '0');
+  const gHex = g.toString(16).padStart(2, '0');
+  const bHex = b.toString(16).padStart(2, '0');
+
+  const color = `#${rHex}${gHex}${bHex}`;
+  
+  return color;
+}
 
 export default function App() {
+  const [color, setColor] = useState(getRandomColor());
+
+  useEffect(() => {
+    let id = setInterval(() => {
+      setColor(getRandomColor());
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: color,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <Text style={{color: "white"}}>Background color { color }</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
