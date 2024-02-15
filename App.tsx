5e0ee426a707df873fa3ea222a84a9b14da8e3f3
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-02-15 09:47:30 +0000

    chore: more update

--- App.tsx
+++ App.tsx
@@ -1,23 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState, JSX } from 'react';
-import { Text, View } from 'react-native';
+import {StatusBar as SBar} from "expo-status-bar";
+import {JSX, useEffect, useState} from "react";
+import {
+    Alert,
+    Button,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    // View
+    // ImageBackground
+    TouchableHighlight,
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Platform,
+    StatusBar,
+} from "react-native";
 
-function getRandomColor() {
+import { useDeviceOrientation } from '@react-native-community/hooks';
+
+function getRandomColor(): string {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
 
-  const rHex = r.toString(16).padStart(2, '0');
-  const gHex = g.toString(16).padStart(2, '0');
-  const bHex = b.toString(16).padStart(2, '0');
+  const rHex = r.toString(16).padStart(2, "0");
+  const gHex = g.toString(16).padStart(2, "0");
+  const bHex = b.toString(16).padStart(2, "0");
 
-  const color = `#${rHex}${gHex}${bHex}`;
-
-  return color;
+  return `#${rHex}${gHex}${bHex}`;
 }
 
 export default function App(): JSX.Element {
-  const [color, setColor] = useState(getRandomColor());
+  const [color, setColor] = useState<string>(getRandomColor());
+
+    const dem = useDeviceOrientation();
+    console.log({dem});
 
   useEffect(() => {
     let id = setInterval(() => {
@@ -30,14 +48,104 @@ export default function App(): JSX.Element {
   }, []);
 
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: color,
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
-      <Text style={{color: "white"}}>Background | running color { color } Tomiwa and Beccy</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ ...style.container, backgroundColor: color }}>
+      <Text
+        style={{ color: "white" }}
+        numberOfLines={1}
+        onPress={() => console.log("Text clicked")}
+      >
+        Background | running color {color} Tomiwa and Beccy and melonny
+      </Text>
+      <TouchableWithoutFeedback
+        onPress={() => console.log("ON PRESS, IMAGE TAPPED")}
+        onLongPress={() => console.log("PRESSED FOR LONGER DURATION")}
+      >
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+          blurRadius={0}
+          fadeDuration={1000}
+          resizeMode="cover"
+        />
+      </TouchableWithoutFeedback>
+
+      <TouchableOpacity
+        onPress={() => console.log("ON PRESS, IMAGE TAPPED")}
+        onLongPress={() => console.log("PRESSED FOR LONGER DURATION")}
+      >
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+          blurRadius={0}
+          fadeDuration={1000}
+          resizeMode="cover"
+        />
+      </TouchableOpacity>
+
+      <Button
+        title="click me"
+        color={color}
+        onPress={() =>
+          // Alert.alert("Question", "Are you normal", [
+          //   { text: "yes", onPress: () => console.log("YES WAS CLICKED") },
+          //   { text: "no", onPress: () => console.log("NO WAS CLICKED") },
+          // ])
+          Alert.prompt("Question", "Are you normal", (text) => {
+            console.log({ text });
+          })
+        }
+      />
+
+      <TouchableHighlight
+        onPress={() => console.log("ON PRESS, IMAGE TAPPED")}
+        onLongPress={() => console.log("PRESSED FOR LONGER DURATION")}
+      >
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+          blurRadius={0}
+          fadeDuration={1000}
+          resizeMode="cover"
+        />
+      </TouchableHighlight>
+
+      <TouchableNativeFeedback
+        onPress={() => console.log("ON PRESS, IMAGE TAPPED")}
+        onLongPress={() => console.log("PRESSED FOR LONGER DURATION")}
+      >
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+          blurRadius={0}
+          fadeDuration={1000}
+          resizeMode="cover"
+        />
+      </TouchableNativeFeedback>
+      <SBar style="auto" />
+    </SafeAreaView>
   );
 }
+
+// Dimension.get("screen"|"window")
+// orientation| default, portrait, landscape
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+      paddingTop: Platform.OS == "android" ? StatusBar.currentHeight: 0,
+  },
+});
